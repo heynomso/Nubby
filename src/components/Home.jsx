dd20037f2db4250de9de16bfac3d113fec7e84c9
Nomso <nomsounaogu@gmail.com>
2023-12-20 20:27:26 +0100

    modified:   src/App.jsx modified:   src/components/Home.jsx modified:   vite.config.js new file:   yarn.lock

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
-import "../scss"
 
 function Home(){
   return (
     <div>
+      <div className="header">
+        <p>NU<span>BB</span>Y</p>
+        <span>Join Nubby</span>
+      </div>
+      <div className="home-vendor-box">
+        <div className="home-vendor-box text-box">
+          <p>Using NU<span>BB</span>Y,</p>
+          <p>find the product or service</p>
+          <p>you need on campus</p>
+        </div>
 
+        <div className="home-vendor-box search-box">
+          <div className="home-vendor-box search-bar"> Search for a product, servcie or vendor</div>
+          <button>Search</button>
+        </div>
+      </div>
+
+      <div className="advertisement">
+        <div className="advertisement text-box">
+          <p>Want to sell it,go for it</p>
+          <p>You need it, we've got it</p>
+        </div>
+
+        <div className="advertisement icons">  
+        </div>
+      </div>
     </div>
   );
 }
