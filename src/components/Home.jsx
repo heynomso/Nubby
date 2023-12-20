a6b6f953d9b2f329f80f0e6331e3194b7d955391
Nomso <nomsounaogu@gmail.com>
2023-12-20 22:30:35 +0100

    new file:   public/images/arrow-down.png new file:   public/images/facebook.png new file:   public/images/instagram.png new file:   public/images/linkedin.png modified:   src/components/Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ function Home(){
     <div>
       <div className="header">
         <p>NU<span>BB</span>Y</p>
-        <span>Join Nubby</span>
+        <button>Join Nubby</button>
       </div>
       <div className="home-vendor-box">
         <div className="home-vendor-box text-box">
@@ -57,6 +57,29 @@ function Home(){
 
          <p><img src="/images/check-ring.png" alt="" />  Break into the market with ease</p>
          <span>As a new student vendor, it can be difficult to get customers, Nubby is here to help you make those sales within campus</span>
+      </div>
+
+      <div className="business-starter">
+        <div className="business-starter center">
+          <p>Early business starter?</p>
+          <p>Hey <span>WE</span> got you</p>
+          <button>Join Nubby</button>
+        </div>
+      </div>
+
+      <div className="accordion">
+        <div className="accordion categories"><p>Categories <img src="images/arrow-down.png" alt="" /></p></div>
+        <div className="accordion categories"><p>About <img src="images/arrow-down.png" alt="" /></p></div>
+        <div className="accordion categories"><p>Become a Student Vendor <img src="images/arrow-down.png" alt="" /></p></div>
+      </div>
+
+      <div className="socials">
+        <div className="socials logo">
+          <img src="images/logo.png" alt="" /><p>  NU<span>BB</span>Y</p>
+        </div>
+          <img src="images/facebook.png" alt="" />
+          <img src="images/instagram.png" alt="" />
+          <img src="images/linkedin.png" alt="" />
       </div>
     </div>
   );
